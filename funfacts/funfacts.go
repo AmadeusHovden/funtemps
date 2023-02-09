1f8c84d65462f7d5cb8666fe7da96eab2b4c73f8
AmadeusHovden <amadeusmh@uia.no>
2023-02-09 13:22:00 +0100

    fun facts test

--- funfacts/funfacts.go
+++ funfacts/funfacts.go
@@ -1,14 +1,39 @@
 package funfacts
 
-/**
-  Implementer funfacts-funksjon:
-    GetFunFacts(about string) []string
-      hvor about kan ha en av tre testverdier, -
-        sun, luna eller terra
-  Sett inn alle Funfucts i en struktur
-  type FunFacts struct {
-      Sun []string
-      Luna []string
-      Terra []string
-  }
-*/
+type FunFacts struct {
+	Sun   []string
+	Luna  []string
+	Terra []string
+}
+
+var funFacts = FunFacts{
+	Sun: []string{
+		"Visste du at temperaturen i solens kjerne er 15 000 000℃",
+		"Visste du at temperaturen på ytre lag av solen er 5778K",
+	},
+	Terra: []string{
+		"Visste du at høyeste målt temperatur på jordens overflate er 56℃",
+		"Visste du at laveste temperatur på jordens overflate er -89.4℃",
+		"Visste du at temperaturen i jordens indre kjerne er 9392K ",
+	},
+	Luna: []string{
+		"Visste du at temperaturen på månens overflate om natten -183℃",
+		"Visste du at temperaturen på månens overflate om dagen 106℃",
+	},
+}
+
+func GetFunFacts(about string) []string {
+	switch about {
+	case "Sun":
+		return funFacts.Sun
+
+	case "Terra":
+		return funFacts.Terra
+
+	case "Luna":
+		return funFacts.Luna
+
+	default:
+		return []string{"ingen funfacts"}
+	}
+}
--- funfacts/funfacts_test.go
+++ funfacts/funfacts_test.go
@@ -5,22 +5,35 @@ import (
 	"testing"
 )
 
-/*
-*
-	Mal for TestGetFunFacts funksjonen.
-	Definer korrekte typer for input og want,
-	og sette korrekte testverdier i slice tests.
-*/
 func TestGetFunFacts(t *testing.T) {
-	type test struct {
-		input // her må du skrive riktig type for input
-		want  // her må du skrive riktig type for returverdien
+	funFacts := FunFacts{
+
+		Sun: []string{
+			"Visste du at temperaturen i solens kjerne er 15 000 000℃",
+			"Visste du at temperaturen på ytre lag av solen er 5778K",
+		},
+		Terra: []string{
+			"Visste du at høyeste målt temperatur på jordens overflate er 56℃",
+			"Visste du at laveste temperatur på jordens overflate er -89.4℃",
+			"Visste du at temperaturen i jordens indre kjerne er 9392K ",
+		},
+		Luna: []string{
+			"Visste du at temperaturen på månens overflate om natten -183℃",
+			"Visste du at temperaturen på månens overflate om dagen 106℃",
+		},
 	}
 
-	// Her må du legge inn korrekte testverdier
-	//tests := []test{
-	//  {input: , want: },
-	//}
+	type test struct {
+		input string
+		want  []string
+	}
+
+	tests := []test{
+		{"sun", funFacts.Sun},
+		{"luna", funFacts.Luna},
+		{"terra", funFacts.Terra},
+		{"unknown", nil},
+	}
 
 	for _, tc := range tests {
 		got := GetFunFacts(tc.input)
